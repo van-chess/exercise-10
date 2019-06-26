0cbdfb18c2f69833ee4708dade9b3ff20d31d697
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-06-26 08:12:01 +0200

    counting threads

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 import random
 import unittest
+import threading
 from ex10_1 import sort
 
+maxNumberOfThreads = threading.active_count()
+sortRunning = True
+
+def maxThreadNumber():
+    while sortRunning:
+        if threading.active_count() > maxNumberOfThreads:
+            maxNumberOfThreads = threading.active_count()
+
 class TestMethods(unittest.TestCase):
+
 
     def testSort(self):
         self.assertEqual(sort([]), [])
@@ -10,6 +20,12 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(sort([2,3,1]), [1,2,3])
         self.assertEqual(sort([1, 2, 3]), [1,2,3])
         self.assertEqual(sort([7, 4, 1,5,2,9]), [1,2,4,5,7,9])
+        
+        t = threading.Thread(target=maxThreadNumber)
+        t.start()
+        self.assertEqual(sort([7, 4, 1,5,2,9]), [1,2,4,5,7,9])
+        sortRunning = False
+        self.assertGreater(maxNumberOfThreads, 1)
     
 
 if __name__ == '__main__':
