8c3aefeb742f1451f904af863ddf0a2d3bbad749
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-06-26 08:20:40 +0200

    added comments to test

--- test.py
+++ test.py
@@ -4,11 +4,12 @@ import threading
 from ex10_1 import sort
 
 maxNumberOfThreads = 1
-sortRunning = True
+runMonitor = True
 
+# not very elegant ...
 def maxThreadNumber():
     global maxNumberOfThreads
-    while sortRunning:
+    while runMonitor:
         if threading.active_count() > maxNumberOfThreads:
             maxNumberOfThreads = threading.active_count()
 
@@ -16,18 +17,25 @@ class TestMethods(unittest.TestCase):
 
 
     def testSort(self):
-        global sortRunning
+        global runMonitor
         self.assertEqual(sort([]), [])
         self.assertEqual(sort([2,3,4,1]), [1,2,3,4])
         self.assertEqual(sort([2,3,1]), [1,2,3])
         self.assertEqual(sort([1, 2, 3]), [1,2,3])
         self.assertEqual(sort([7, 4, 1,5,2,9]), [1,2,4,5,7,9])
         
+        # starts a separate monitor thread that stores the 
+        # number of running threads
         t = threading.Thread(target=maxThreadNumber)
         t.start()
+        # run a sorting
         self.assertEqual(sort([7, 4, 1,5,2,9]), [1,2,4,5,7,9])
-        sortRunning = False
+        # monitor thread can stop now
+        runMonitor = False
+        # wait for the monitor thread to actually stop
         t.join()
+        # the number of threads running at a time should be higher than 2
+        # (i.e., at least one other thread has been created at some point)
         self.assertGreater(maxNumberOfThreads, 2)
     
 
